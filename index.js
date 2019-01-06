678418cf4d06b8c87a17910fc95c8518500f8c77
H-OO <hjyuanyy@gmail.com>
2019-01-07 02:31:14 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// 实现require方法
-function _require(path) {
-  console.log(path);
-  const filename = __dirname + '/' + path;
-  console.log(filename)
-}
-const a = _require('./module/a.js');
+const path = require('path');
+const res = path.win32;
+console.log('')
