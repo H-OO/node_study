49c5112c8005099499081e02dd3fa2eb475943f8
H-OO <38312623+H-OO@users.noreply.github.com>
2019-01-18 20:14:44 +0800

    update

--- app/index.js
+++ app/index.js
@@ -1,35 +1,11 @@
-const { MongoClient } = require('mongodb');
-const url = 'mongodb://127.0.0.1:27017';
-const dbName = 'dev';
-MongoClient.connect(
-  url,
-  {
-    useNewUrlParser: true
-  },
-  (err, client) => {
-    if (err) throw err;
-    const db = client.db(dbName);
-    // 通过 `db` 进行增删改查
-    // client.close(); // 操作完成断开连接
-    db.collection('test').insertMany(
-      [
-        {
-          name: 'a',
-          age: 1
-        },
-        {
-          name: 'a',
-          age: 2
-        }
-      ],
-      err => {
-        if (err) {
-          console.log('error');
-          return;
-        }
-        console.log('success');
-        client.close(); // 断开连接
-      }
-    );
-  }
-);
+const Koa = require('koa');
+// 实例化
+const app = new Koa();
+
+// 使用中间件
+app.use(async (ctx) => {
+  ctx.body = 'Hello koa2.x';
+});
+
+// 监听
+app.listen(8888);
